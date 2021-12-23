212bd355cf6a73f04964db6bdbe2b662d28f93f3
Sebastian Key <sebastianwkey@gmail.com>
2021-12-22 18:08:13 -0800

    Style menu board

---
+++ nextjs-example/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      turquoise: "#24C5BE",
+      darkGray: "#363636",
+    },
+    fontFamily: {
+      "vesper-libre": '"Libre Baskerville"',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
