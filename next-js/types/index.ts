b4be60e203d8ccb9402e52f4fb65c7c1bb2977fb
Sebastian Key <sebastianwkey@gmail.com>
2022-03-15 12:48:04 +1100

    Replace hardcoded data with Toast data

--- next-js/types/index.ts
+++ next-js/types/index.ts
@@ -1,29 +1,16 @@
 export interface MenuData {
   id: string;
-  groups: Group[];
-}
-
-export interface Group {
-  id: string;
   name: string;
   description: string;
-  price: string;
   items: Item[];
-  modifierGroups: ModifierGroup[];
 }
 
+export type Tag = string;
 export interface Item {
   id: string;
   name: string;
   description: string;
-  price: string;
   calories: string;
-}
-
-export interface ModifierGroup {
-  id: string;
-  name: string;
-  description: string;
   price: string;
-  items: Item[];
+  tags: Tag[];
 }
