94011f31042707915c96d43bb626bf3ae8b3f009
Sebastian Key <sebastianwkey@gmail.com>
2022-03-30 17:53:42 +1100

    Set cache-control headers

--- gotts-menu/next.config.js
+++ gotts-menu/next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "*",
+        source: "/:path*",
         headers: [
           {
             key: "Cache-Control",
